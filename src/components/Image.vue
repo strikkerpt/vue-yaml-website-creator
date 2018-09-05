f305129c32de2c17a78bbb0fca48343bc2c3b41b
Sinésio Couceiro <scouceiro@thing-pink.pt>
2018-09-05 21:26:02 +0100

    [fix] Fixed image block being always called and require static files

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="col-12">
-        <img class="img-fluid" :src="src" :alt="title"/>
+        <img class="img-fluid" :src="srcFile" :alt="title"/>
     </div>
 </template>
 
 <script>
     export default {
       name: 'Imageblock',
-      data: function () {
-        return {
-          'image': '',
-        }
-      },
       props: ['src', 'title'],
+      computed: {
+        srcFile() {
+          if (this.$props.src.includes('http')) {
+            return this.$props.src;
+          } else {
+            return require('./../assets/' + this.$props.src);
+          }
+        }
+      }
     }
 </script>
